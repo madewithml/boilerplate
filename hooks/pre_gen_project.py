ca4841e28c09e42998fdfa37159386b06f1b57af
Goku Mohandas <gokumd@gmail.com>
2019-03-16 15:34:56 -0700

    testing context injection

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,20 +1,5 @@
 import sys
-from cookiecutter.main import cookiecutter
 
-service_name = "{{ cookiecutter.service_name }}"
-service_name = service.replace("_", "-")
-service_name = service.replace(" ", "-")
-
-package_name = service_name.replace("-", "_")
-
-cookiecutter(
-    extra_context={"package_name": package_name}
-)
-
-print (1)
-print (service_name, package_name)
-
-print (2)
 print ("{{ cookiecutter.service_name }}", "{{ cookiecutter.package_name }}")
 
 sys.exit()
